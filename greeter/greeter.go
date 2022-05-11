e9b71b596895fd290de004f973a59cc26c5febfb
Aidar Mukhametov <2mai2@bk.ru>
2022-05-12 00:32:37 +0500

    fixed bugs

--- greeter/greeter.go
+++ greeter/greeter.go
@@ -10,10 +10,12 @@ func Greet(name string, hour int) string {
 
 	if hour >= 6 && hour < 12 {
 		greeting = "Good morning"
-	} else if hour >= 12 && hour <= 18 {
+	} else if hour >= 12 && hour < 18 {
 		greeting = "Hello"
 	} else if hour >= 18 && hour < 22 {
 		greeting = "Good evening"
+	} else if hour < 0 || hour > 24 {
+		greeting = "Hello"
 	}
 	trimmedName := strings.Trim(name, " ")
 	return fmt.Sprintf("%s %s!", greeting, strings.Title(trimmedName))
