77c55ef33f31237568f635db23bb6036f8b6aa71
Aidar Mukhametov <2mai2@bk.ru>
2022-05-14 15:55:59 +0500

    new tests gRPC

---
+++ tests/create_device_grpc_test.go
@@ -0,0 +1,74 @@
+package tests
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	act_device_api "github.com/ozonmp/act-device-api/pkg/act-device-api"
+	"google.golang.org/grpc"
+	"testing"
+)
+
+func TestCreateDevice(t *testing.T) {
+	ctx := context.Background()
+	host := "localhost:8082"
+	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("grpc.Dial err:%v", err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			t.Logf("conn.Close err:%v", err)
+		}
+	}(conn)
+
+	t.Run("CD1 CreateDevice create device with correct data", func(t *testing.T) {
+
+		platform := "Win10"
+		userId := 111222333
+		request := &act_device_api.CreateDeviceV1Request{ // передаем номер устройства
+			Platform: platform,
+			UserId:   uint64(userId),
+		}
+
+		createDevicesV1Response, err := act_device_api.NewActDeviceApiServiceClient(conn).CreateDeviceV1(ctx, request)
+		assert.Equal(t, codes.OK.String(), status.Code(err).String()) //  проверяем статус код
+		require.NoError(t, err)                                       // отсутствуют ошибок в ответе
+		require.NotNil(t, createDevicesV1Response)                    // ответ не пустой
+		// сравним полученный ответ с ожидаемым
+		assert.NotNil(t, createDevicesV1Response.DeviceId) // не пусто
+		t.Logf("DeviceId %v", createDevicesV1Response.DeviceId)
+	})
+
+	t.Run("CD2 CreateDevice create device without platform", func(t *testing.T) {
+
+		platform := ""
+		userId := 222333
+		request := &act_device_api.CreateDeviceV1Request{ // передаем номер устройства
+			Platform: platform,
+			UserId:   uint64(userId),
+		}
+
+		_, err := act_device_api.NewActDeviceApiServiceClient(conn).CreateDeviceV1(ctx, request)
+		assert.Equal(t, codes.InvalidArgument.String(), status.Code(err).String()) //  проверяем статус код
+
+	})
+
+	t.Run("CD3 CreateDevice create device without userId", func(t *testing.T) {
+
+		platform := "Win10"
+		var userId uint64
+		request := &act_device_api.CreateDeviceV1Request{ // передаем номер устройства
+			Platform: platform,
+			UserId:   userId,
+		}
+
+		_, err := act_device_api.NewActDeviceApiServiceClient(conn).CreateDeviceV1(ctx, request)
+		assert.Equal(t, codes.InvalidArgument.String(), status.Code(err).String()) //  проверяем статус код
+
+	})
+}
---
+++ tests/list_devices_grpc_test.go
@@ -0,0 +1,65 @@
+package tests
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	act_device_api "github.com/ozonmp/act-device-api/pkg/act-device-api"
+	"google.golang.org/grpc"
+	"testing"
+)
+
+func TestListDevice(t *testing.T) {
+	ctx := context.Background()
+	host := "localhost:8082"
+	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("grpc.Dial err:%v", err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			t.Logf("conn.Close err:%v", err)
+		}
+	}(conn)
+
+	t.Run("LD1 ListDevice display device on page with a given number entry", func(t *testing.T) {
+
+		page := uint64(1)
+		perPages := uint64(5)
+		request := &act_device_api.ListDevicesV1Request{
+			Page:    page,
+			PerPage: perPages,
+		}
+
+		listDevicesV1Response, err := act_device_api.NewActDeviceApiServiceClient(conn).ListDevicesV1(ctx, request)
+		assert.Equal(t, codes.OK.String(), status.Code(err).String())
+		require.NoError(t, err)
+		require.NotNil(t, listDevicesV1Response)
+		assert.Equal(t, len(listDevicesV1Response.Items), int(perPages)) // кол-во записей = perPage
+		for _, value := range listDevicesV1Response.Items {              //перебираем тела ответов
+			require.NotEmpty(t, value.Platform) // поле платформа не пустое
+			require.NotEmpty(t, value.UserId)   // поле идентификатор пользователя не пустое
+		}
+
+	})
+
+	t.Run("LD2 ListDevice incorrect request", func(t *testing.T) {
+		var x uint64 // пустое значение
+		page := []uint64{1, x, x}
+		perPages := []uint64{x, 1, x}
+		for i := 0; i < len(page); i++ {
+			request := &act_device_api.ListDevicesV1Request{
+				Page:    page[i],
+				PerPage: perPages[i],
+			}
+
+			listDevicesV1Response, _ := act_device_api.NewActDeviceApiServiceClient(conn).ListDevicesV1(ctx, request)
+			assert.Emptyf(t, listDevicesV1Response.GetItems(), "listDevicesV1Response.Items - не пустой")
+
+		}
+	})
+}
